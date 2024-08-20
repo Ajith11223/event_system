8a3c3b559b035a702860f927a8a3736f14327dc7
Ajith11223 <hashmiajith@gmail.com>
2024-08-20 19:57:46 +0530

    error fix

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,20 +1,26 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const MONGODB_URI = 'mongodb+srv://ajithm12aji:aGWLtv0xuNuSpAka@eventdb.us6k5.mongodb.net/?retryWrites=true&w=majority&appName=eventDB'
-
+const MONGODB_URI =
+  "mongodb+srv://ajithm12aji:aGWLtv0xuNuSpAka@eventdb.us6k5.mongodb.net/?retryWrites=true&w=majority&appName=eventDB";
 
 let isConnected;
 
 export default async function connectToDatabase() {
-  if (isConnected) {
-    return;
+  // if (isConnected) {
+  //   return;
+  // }
+
+  // const db = await mongoose.connect(MONGODB_URI, {
+  //   useNewUrlParser: true,
+  //   useUnifiedTopology: true,
+  // });
+
+  // isConnected = db.connections[0].readyState;
+  if (mongoose.connection.readyState === 1) {
+    return mongoose.connection.asPromise();
+  } else {
+    const url = MONGODB_URI;
+
+    mongoose.connect(url);
   }
-
-  const db = await mongoose.connect(MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-
-  isConnected = db.connections[0].readyState;
 }
-
